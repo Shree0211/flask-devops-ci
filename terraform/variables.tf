490d3cbfe5282a7b525b0056ae673ff0e14d1873
shree0211 <shreevyas0211@gmail.com>
2026-01-22 14:05:44 +0530

    Fix terraform auto-approve

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,17 +3,18 @@ variable "aws_region" {
   type        = string
 }
 
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed to SSH"
+  type        = string
+}
+
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
+  default     = "t2.micro"
 }
 
 variable "key_name" {
   description = "EC2 key pair name"
-  type        = string
-}
-
-variable "allowed_ssh_cidr" {
-  description = "Allowed CIDR for SSH"
   type        = string
 }
